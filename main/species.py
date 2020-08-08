e58bc06ec1850e4448715b2b812d975ad7206b1f
Ta7ar <56931453+Ta7ar@users.noreply.github.com>
2020-08-07 23:56:06 -0400

    added mutate and crossover methods

--- main/species.py
+++ main/species.py
@@ -1,5 +1,7 @@
 import numpy as np
 import cv2
+import random
+from typing import Tuple
 
 
 class Specie:
@@ -15,29 +17,29 @@ class Specie:
 
      """
 
-    def __init__(self, target_image, genes=128, mutation_rate=0.01):
+    def __init__(self, target_image: np.ndarray, genes: int = 128, mutation_rate: float = 0.01):
         """
         Attributes:
             target_image(np.ndarray): The target image the class is to be trained on
             genes(Int): The number of circles used to create a trained image
             mutation_rate(float): The chance of a gene being altered on the creation of a new generation
         """
-        self.target_image = target_image
-        self.shape = target_image.shape
-        self.genes = genes
-        self.mutation_rate = mutation_rate
-        self.genotype = np.random.rand(genes, 5)
+        self.target_image: np.ndarray = target_image
+        self.shape: Tuple[int, int] = target_image.shape
+        self.genes: int = genes
+        self.mutation_rate: float = mutation_rate
+        self.genotype: np.ndarray = np.random.rand(genes, 5)
 
         # initial value a placeholder/ blank black canvas
-        self.phenotype = np.zeros(target_image.shape)
-        self.fitness = 0
+        self.phenotype: np.ndarray = np.zeros(target_image.shape)
+        self.fitness: float = 0
 
-        self.average_radius = (self.shape[1] + self.shape[0])/2 / 6
+        self.average_radius: float = (self.shape[1] + self.shape[0])/2 / 6
 
-        self.max_error = (np.square((1 - (self.target_image >= 127))
-                                    * 255 - self.target_image)).mean(axis=None)
+        self.max_error: float = (np.square((1 - (self.target_image >= 127))
+                                           * 255 - self.target_image)).mean(axis=None)
 
-    def gen_phenotype(self):
+    def gen_phenotype(self) -> None:
         """ Creates an image using the genotype and overwrites self.phenotype
         """
 
@@ -61,9 +63,30 @@ class Specie:
             self.phenotype = cv2.addWeighted(
                 overlay, alpha, self.phenotype, 1 - alpha, 0).astype(int)
 
-    def score(self):
+    def score(self) -> None:
         """ Sets the fitness score of the specie based on current phenotype.
         Scored based on Mean Squared Error"""
-        cost = int(np.mean(np.square(self.target_image - self.phenotype)))
+        cost: int = int(np.mean(np.square(self.target_image - self.phenotype)))
         self.fitness = (self.max_error - cost) / self.max_error
-        
+
+    def crossover(self, parentB) -> np.ndarray:
+        """ Carries out genetic crossover between two parent species
+            Arguments:
+                ParentB(Specie): The specie class instance with which crossover takes place.
+            Returns:
+                Child specie instance of Specie class.
+        """
+
+        # midpoint method, fast but inaccurate
+        midpoint = np.random.randint(1, self.genes)
+        parentA_segment = self.genotype[:midpoint]
+        parentB_segment = parentB.genotype[midpoint:]
+        child = np.concatenate((parentA_segment, parentB_segment), axis=0)
+        return child
+
+    def mutate(self) -> None:
+        """ Randomly mutates genes in genotype
+        """
+        for i in range(self.genes):
+            if (random.uniform(0, 1) < self.mutation_rate):
+                self.genotype[i] = np.random.rand(5)
